e189d738d7812a3bce30807c90d3ef404d1bc4cc
Jasmin <jasmin.husadzic@aplaceformom.com>
2021-11-12 22:32:14 +0100

    add headless mode

--- wdio.local.conf.js
+++ wdio.local.conf.js
@@ -15,7 +15,7 @@ exports.config = Object.assign(common, {
     'goog:chromeOptions': {
     // to run chrome headless the following flags are required
     // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-    args: [/*'--headless',*/ '--disable-gpu', '--incognito'],
+    args: ['--headless', '--disable-gpu', '--incognito'],
     }
   }],
 
